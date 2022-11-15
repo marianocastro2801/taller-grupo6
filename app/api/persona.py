362908f07b348d3131ca5eeba756d9e599a19960
MAXIMILIANO LEONELO FRANCESCONI <maxifni_fsa@live.com.ar>
2022-11-15 15:33:28 -0300

    se comento lo de la api pq no funciona bien

--- app/api/persona.py
+++ app/api/persona.py
@@ -1,17 +1,17 @@
-from lib2to3.refactor import get_all_fix_names
-from flask import jsonify, Blueprint, request, abort
-from marshmallow import ValidationError
-import werkzeug
-from app.schemas.persona import persona_schema
-from sqlalchemy.exc import SQLAlchemyError
+# from lib2to3.refactor import get_all_fix_names
+# from flask import jsonify, Blueprint, request, abort
+# from marshmallow import ValidationError
+# import werkzeug
+# from app.schemas.persona import persona_schema
+# from sqlalchemy.exc import SQLAlchemyError
 
-from app.schemas.persona import persona_schema
+# from app.schemas.persona import persona_schema
 
-persona_api = Blueprint("personas", __name__, url_prefix="/personas")
+# persona_api = Blueprint("personas", __name__, url_prefix="/personas")
 
 
-@persona_api.get("https://api.claudioraverta.com/personas")
-def index():
+# @persona_api.get("https://api.claudioraverta.com/personas")
+# def index():
 
-    json = 1
-    return  json
+#     json = 1
+#     return  json
