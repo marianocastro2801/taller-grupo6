c0a3859d888fb19a9814bb1a0fd53f1ad474b2a4
MAXIMILIANO LEONELO FRANCESCONI <maxifni_fsa@live.com.ar>
2022-12-20 18:25:23 -0300

    se agrego la vacunacion de quintuple o pentavalente

--- app/resources/vacunattion.py
+++ app/resources/vacunattion.py
@@ -206,7 +206,49 @@ def save():
                     flash("el paciente no cumple la condicion de los 4 meses ")                 
                     return redirect(url_for("vacunattiones.vacunattion_index"))     
         if unicaDosis != '1' and unicaDosis != '2':
-            flash("La vacuna Rotavirus corresponde a : Primera Dosis y  Segunda Dosis")
+            flash("La vacuna Neumococo Conjugada corresponde a : Primera Dosis y  Segunda Dosis")
+            return redirect(url_for("vacunattiones.vacunattion_index"))
+
+#----------------------------------------------------------------------------
+#pentavalente o quíntuple
+    dentro_dos_meses = paciente.fecha_nacimiento + relativedelta(months=2)
+    dentro_cuatro_meses = paciente.fecha_nacimiento + relativedelta(months=4)
+    dentro_seis_meses = paciente.fecha_nacimiento + relativedelta(months=6)
+    # EL USUARIO ELIGIO pentavalente o quíntuple y SON 3 DOSIS
+    if e == '157':
+        if  unicaDosis == '1':
+            if format(f) <= str(dentro_dos_meses) :
+                new_vacunattion = request.form.copy()
+                new_vacunattion.pop("id", None) 
+                Vacunattion(**new_vacunattion).save()
+                flash("Se ha registrado la vacunacion exitosamente de pentavalente o quíntuple al paciente de 2 meses") 
+                return redirect(url_for("vacunattiones.vacunattion_index"))
+            elif unicaDosis == '1': 
+                    flash("el paciente no cumple la condicion de los 2 meses ")                 
+                    return redirect(url_for("vacunattiones.vacunattion_index"))
+        else:
+             if unicaDosis == '2':
+                if format(f) <= str(dentro_cuatro_meses) :
+                    new_vacunattion = request.form.copy()
+                    new_vacunattion.pop("id", None) 
+                    Vacunattion(**new_vacunattion).save()
+                    flash("Se ha registrado la vacunacion exitosamente de pentavalente o quíntuple al paciente de 4 meses") 
+                    return redirect(url_for("vacunattiones.vacunattion_index"))
+                elif unicaDosis == '2': 
+                    flash("el paciente no cumple la condicion de los 4 meses ")                 
+                    return redirect(url_for("vacunattiones.vacunattion_index"))
+        if  unicaDosis == '3':
+            if format(f) <= str(dentro_seis_meses) :
+                    new_vacunattion = request.form.copy()
+                    new_vacunattion.pop("id", None) 
+                    Vacunattion(**new_vacunattion).save()
+                    flash("Se ha registrado la vacunacion exitosamente de pentavalente o quíntuple al paciente de 6 meses") 
+                    return redirect(url_for("vacunattiones.vacunattion_index"))
+            elif unicaDosis == '3': 
+                    flash("el paciente no cumple la condicion de los 6 meses ")                 
+                    return redirect(url_for("vacunattiones.vacunattion_index")) 
+        if unicaDosis != '1' and unicaDosis != '2' and unicaDosis != '3' :
+            flash("La vacuna pentavalente o quíntuple corresponde a : Primera Dosis , Segunda Dosis y Tercera Dosis")
             return redirect(url_for("vacunattiones.vacunattion_index"))
 
 
